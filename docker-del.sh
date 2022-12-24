6a7d2c5245c549d88a26df2d4cadf265704c0f88
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-12-24 19:01:26 +0530

    Fixed not extracting first rows

--- docker-del.sh
+++ docker-del.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 # Remove all docker container
-for con in $(sudo docker ps -a | awk {'print $1'});do
+for con in $(sudo docker ps -a | tail -n +2 | awk {'print $1'});do
 	sudo docker rm $con
 done
 
 # Remove all docker images
-for img in $(sudo docker images | awk {'print $3'});do
+for img in $(sudo docker images | tail -n +2 | awk {'print $3'});do
 	sudo docker rmi $img
 done
